9a448b7bf2d1dad3ee4657e3ae9f1df0fb9481d2
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2026-04-17 12:15:01 +0530

    Updated backend URL for production

--- frontend/vite-project/src/socket.js
+++ frontend/vite-project/src/socket.js
@@ -1,6 +1,6 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001';
+export const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 const socket = io(BACKEND_URL, {
   autoConnect: false,
