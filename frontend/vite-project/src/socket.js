2f6ab725168a429cd417cd981d96c7b98614c11e
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2026-04-17 12:20:22 +0530

    Fixed socket redeclaration error

--- frontend/vite-project/src/socket.js
+++ frontend/vite-project/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_BACKEND_URL);
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const socket = io(BACKEND_URL, {
   autoConnect: false,
